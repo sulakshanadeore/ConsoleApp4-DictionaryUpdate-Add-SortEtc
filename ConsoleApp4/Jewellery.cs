e657a36f4164ac5feceeecdf39176a8d98c363cd
deoresulakshana <deoresulakshana@gmail.com>
2024-11-16 10:22:43 +0530

    Add project files.

---
+++ ConsoleApp4/Jewellery.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+   public class JewelleryUtility:Jewellery
+    {
+
+
+
+
+        public Dictionary<int, Jewellery> AddNewJewellery(Jewellery toadd)
+        {
+
+            int cnt=Program.jewellerydetails.Count;
+            
+            Program.jewellerydetails.Add(cnt + 1, toadd);
+            
+            
+            return Program.jewellerydetails;
+
+
+        }
+        public Dictionary<int, Jewellery> UpdateJewelleryDetails(string id,Jewellery toupdate)
+        {
+
+            //Dictionary<string, string> newdict = new Dictionary<string, string>();
+
+
+            string s = null;
+            for (int i = 0; i < Program.jewellerydetails.Count; i++)
+            {
+                foreach (KeyValuePair<int, Jewellery> item in Program.jewellerydetails)
+                {
+                    if (item.Value.Id == id)
+                    {
+                        //s = string.Concat(item.Value.Material, "_", item.Value.Type);
+                        item.Value.Id=toupdate.Id;
+                        item.Value.Material = toupdate.Material; 
+                        item.Value.Price = toupdate.Price;
+                        item.Value.Type = toupdate.Type;
+
+
+
+                    }
+
+                }
+                
+                
+            }
+            return Program.jewellerydetails;
+
+
+        }
+
+
+
+        public void SortData()
+        {
+            var data = from p in Program.jewellerydetails
+                       group p by p.Value.Material;
+
+            foreach (var item in data)
+            {
+                Console.WriteLine(item.Key.ToString());
+                Console.WriteLine("--------------------");
+
+                foreach (var item1 in item)
+                {
+                    Console.WriteLine(item1.Value.Type);
+                    Console.WriteLine(item1.Value.Id);
+                    Console.WriteLine(item1.Value.Price);
+                    Console.WriteLine(item1.Value.Material);
+                    Console.WriteLine();
+                }
+            }
+
+
+
+            //var d=from d1 in Program.jewellerydetails.Values
+            //      orderby d1.Id
+            //      select d1;
+
+            //foreach (var item in d)
+            //{
+            //    Console.WriteLine(item.Id);
+            //    Console.WriteLine(item.Material);
+            //    Console.WriteLine(item.Type);
+            //    Console.WriteLine(item.Price);
+            //    Console.WriteLine(  );
+            //}
+
+
+
+
+        }
+
+
+
+
+    
+
+
+        public Dictionary<string, string> GetJewelleryDetails(string id)
+        {
+
+            Dictionary<string, string> newdict = new Dictionary<string, string>();
+
+          
+            string s = null;
+           for (int i = 0;i<Program.jewellerydetails.Count;i++)
+            {
+                foreach (KeyValuePair<int, Jewellery> item in Program.jewellerydetails)
+                {
+                    if (item.Value.Id == id)
+                    {
+                         s = string.Concat(item.Value.Material, "_", item.Value.Type);
+                      
+                       
+                    }
+                    
+                }
+                newdict.Clear();                
+                newdict.Add(id, s);
+            }
+            return newdict;
+
+
+        }
+
+        
+    }
+    
+
+
+
+   public class Jewellery
+    {
+        public string Id { get; set; }
+        public string Type { get; set; }
+        public string Material { get; set; }
+        public int Price { get; set; }
+
+
+    }
+
+
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        public static Dictionary<int, Jewellery> jewellerydetails = new Dictionary<int, Jewellery>();
+          
+
+        static void Main(string[] args)
+        {
+            jewellerydetails.Add(34, new Jewellery { Id = "JW04", Material = "Gold", Price = 12000, Type = "Chain" });
+            jewellerydetails.Add(1, new Jewellery { Id = "JW02", Material = "Silver", Price = 1000, Type = "Bracelet" });
+            jewellerydetails.Add(2, new Jewellery { Id = "JW01", Material = "Gold", Price = 2000, Type = "Chain" });
+            jewellerydetails.Add(4, new Jewellery { Id = "JW03", Material = "Silver", Price = 11000, Type = "Bracelet" });
+
+            JewelleryUtility u= new JewelleryUtility(); 
+            u.SortData();   
+
+
+
+
+
+            //   FindJewellery();
+            //UpdateDictionary();
+            //AddJewellery();
+
+            Console.ReadLine();
+        }
+
+        private static void AddJewellery()
+        {
+            JewelleryUtility u = new JewelleryUtility();
+            Jewellery j = new Jewellery();
+            Console.WriteLine("Enter ID");
+            j.Id = Console.ReadLine();
+            Console.WriteLine("Enter Material");
+            j.Material = Console.ReadLine();
+            Console.WriteLine("Enter type");
+            j.Type = Console.ReadLine();
+            Console.WriteLine("Enter price");
+            j.Price = Convert.ToInt32(Console.ReadLine());
+
+
+            Program.jewellerydetails = u.AddNewJewellery(j);
+            Console.WriteLine("After Adding.............");
+            foreach (KeyValuePair<int, Jewellery> data in Program.jewellerydetails)
+            {
+                Console.WriteLine(data.Key);
+                Console.WriteLine(data.Value.Id);
+                Console.WriteLine(data.Value.Material);
+                Console.WriteLine(data.Value.Price);
+                Console.WriteLine(data.Value.Type);
+
+            }
+        }
+
+        private static void UpdateDictionary()
+        {
+            Jewellery j = new Jewellery();
+            Console.WriteLine("Enter ID");
+            j.Id = Console.ReadLine();
+            Console.WriteLine("Enter Material");
+            j.Material = Console.ReadLine();
+            Console.WriteLine("Enter type");
+            j.Type = Console.ReadLine();
+            Console.WriteLine("Enter price");
+            j.Price = Convert.ToInt32(Console.ReadLine());
+
+            JewelleryUtility utility = new JewelleryUtility();
+            Program.jewellerydetails = utility.UpdateJewelleryDetails(j.Id, j);
+            Console.WriteLine("After updating......................");
+            
+            foreach (KeyValuePair<int, Jewellery> data in Program.jewellerydetails)
+            {
+                Console.WriteLine(data.Key);
+                Console.WriteLine(data.Value.Id);
+                Console.WriteLine(data.Value.Material);
+                Console.WriteLine(data.Value.Price);
+                Console.WriteLine(data.Value.Type);
+
+            }
+        }
+
+        private static void FindJewellery()
+        {
+            Console.WriteLine("Enter id to find");
+            string find = Console.ReadLine();
+            JewelleryUtility u = new JewelleryUtility();
+            Dictionary<string, string> detailsFound = u.GetJewelleryDetails(find);
+            if (detailsFound != null)
+            {
+                Console.WriteLine(detailsFound.ElementAtOrDefault(0).ToString());
+            }
+            else
+            {
+                Console.WriteLine("null");
+            }
+        }
+    }
+}
